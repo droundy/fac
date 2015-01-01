5cfb85e5b899a7076fda8af8fa922f7bbad3aa49
David Roundy <roundyd@physics.oregonstate.edu>
2015-01-01 09:10:02 -0600

    create .bilge file (gratuitously)
    Currently, this requires running ./bilge twice to build.  I intend to
    fix that.

---
+++ top.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2
+
+import string
+
+flags = '-Wall -std=c11 -g'
+
+sources = ['bilge', 'files', 'targets', 'build']
+
+libsources = ['listset', 'bigbrother']
+
+for s in sources:
+    print '| gcc %s -c %s.c' % (flags, s)
+    print '> %s.o' % s
+    print
+
+print '| gcc -o bilge', string.join(['%s.o' % s for s in sources]), string.join(['lib/%s.o' % s for s in libsources])
+for s in sources:
+    print '< %s.o' % s
+for s in libsources:
+    print '< lib/%s.o' % s
+print '> bilge'
