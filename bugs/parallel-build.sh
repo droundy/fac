fba5fc8dbce3f021338fc093c660219410921435
David Roundy <roundyd@physics.oregonstate.edu>
2015-01-04 14:21:04 -0800

    add test case for parallel build

---
+++ bugs/parallel-build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -ev
+
+rm -rf $0.dir
+mkdir $0.dir
+cd $0.dir
+cat > top.bilge <<EOF
+| sleep 2 && echo foo > foo
+> foo
+
+| sleep 2 && echo bar > bar
+> bar
+
+| cat foo bar > foobar
+> foobar
+< foo
+< bar
+EOF
+
+/usr/bin/time -f '%e' ../../bilge 2> bilge.time
+
+grep foo foobar
+grep bar foobar
+
+cat bilge.time
+perl -e 'if ($ARGV[0] > 3) { print "FAIL: $ARGV[0] too big\n"; exit 1; }' `cat bilge.time`
+
+exit 0
