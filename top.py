e976cd66c05144f2aeafdaaab269eb5e47740b44
David Roundy <roundyd@physics.oregonstate.edu>
2015-01-08 16:28:56 -0800

    start work on compiling 32-bit version to test code in 32 bits

--- top.py
+++ top.py
@@ -12,6 +12,9 @@ for s in sources:
     print '| gcc %s -c %s.c' % (flags, s)
     print '> %s.o' % s
     print
+    print '| gcc -m32 %s -o %s-32.o -c %s.c' % (flags, s, s)
+    print '> %s-32.o' % s
+    print
 
 print '| gcc -lpthread -lpopt -o bilge', string.join(['%s.o' % s for s in sources]), string.join(['lib/%s.o' % s for s in libsources])
 for s in sources:
@@ -34,4 +37,20 @@ for test in ctests:
 
     print '| cd tests && gcc %s -c %s.c' % (flags, test)
     print '> tests/%s.o' % test
+    print
+
+ctests = ['arrayset', 'listset', 'spinner']
+
+libsources.remove('bigbrother') # it doesn't work yet with 32 bit
+
+for test in ctests:
+    print '| gcc -m32 -lpthread -o tests/%s-32.test' % test, 'tests/%s-32.o' % test, string.join(['lib/%s-32.o' % s for s in libsources])
+    print '> tests/%s-32.test' % test
+    print '< tests/%s-32.o' % test
+    for s in libsources:
+        print '< lib/%s-32.o' % s
+    print
+
+    print '| cd tests && gcc -m32 %s -o %s-32.o -c %s.c' % (flags, test, test)
+    print '> tests/%s-32.o' % test
     print
