873320fb881b96e383f06742d33fe4b0612e5b1f
David Roundy <roundyd@physics.oregonstate.edu>
2015-01-17 11:29:49 -0800

    add test

---
+++ tests/missing-output-file.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -ev
+
+rm -rf $0.dir
+mkdir $0.dir
+cd $0.dir
+
+cat > top.bilge <<EOF
+| echo foo > foo
+> foo
+> bar
+
+| cat bar > ugly
+< bar
+> ugly
+EOF
+
+if ../../bilge > bilge.out 2>&1; then
+    cat bilge.out
+    echo Bilge was okay.  That is not good.
+    exit 1
+else
+    cat bilge.out
+    echo Bilge failed as it ought.
+fi
+
+if grep 'build failed' bilge.out | grep ugly; then
+    echo we should not have attempted to build ugly in the first place
+    exit 1
+fi
+
+grep 'build failed' bilge.out | grep bar
+
+exit 0
