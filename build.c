9907c162cdae691dc2981c89c077409e42c1ea8d
David Roundy <roundyd@physics.oregonstate.edu>
2015-01-18 15:32:57 -0800

    exit our extra forks using exec rather than exit

--- build.c
+++ build.c
@@ -318,6 +318,16 @@ void let_us_build(struct all_targets **all, struct rule *r, int *num_to_build,
           pid_t new_pid = fork();
           if (new_pid == 0) {
             run_parallel_rule(bs[i]);
+
+            /* The following is extremely hokey and stupid.  Rather
+               than exiting the forked process, we exec /bin/true
+               (which has the same effect, but less efficiently) so
+               that profilers won't get confused by us forking but not
+               calling exec. */
+            char **args = malloc(2*sizeof(char *));
+            args[0] = "/bin/true";
+            args[1] = 0;
+            execv("/bin/true", args);
             exit(0);
           }
           bs[i]->pid = new_pid;
