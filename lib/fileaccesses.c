c8045446622b7be911094cf6c3ccd0272b452107
David Roundy <roundyd@physics.oregonstate.edu>
2015-01-18 14:51:01 -0800

    change fileaccesses to use arrayset just as bilge does.

--- lib/fileaccesses.c
+++ lib/fileaccesses.c
@@ -10,45 +10,30 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  listset *written_to_files = 0;
-  listset *read_from_files = 0;
-  listset *read_from_directories = 0;
-  listset *deleted_files = 0;
+  arrayset written_to_files;
+  arrayset read_from_files;
+  arrayset read_from_directories;
+  arrayset deleted_files;
+  initialize_arrayset(&written_to_files);
+  initialize_arrayset(&read_from_files);
+  initialize_arrayset(&read_from_directories);
+  initialize_arrayset(&deleted_files);
 
   char **args = (char **)malloc(argc*sizeof(char*));
   memcpy(args, argv+1, (argc-1) * sizeof(char*));
   args[argc-1] = NULL;
-  bigbrother_process(".", args, &read_from_directories,
-                     &read_from_files, &written_to_files, &deleted_files);
+  pid_t child;
+  bigbrother_process_arrayset(".", args, &child, &read_from_directories,
+                              &read_from_files, &written_to_files, &deleted_files);
   free(args);
 
-  listset *s = read_from_directories;
-  while (s != NULL) {
-    fprintf(stderr, "l: %s\n", s->path);
-    s = s->next;
-  }
-
-  s = read_from_files;
-  while (s != NULL) {
-    fprintf(stderr, "r: %s\n", s->path);
-    s = s->next;
-  }
-
-  s = written_to_files;
-  while (s != NULL) {
-    fprintf(stderr, "w: %s\n", s->path);
-    s = s->next;
-  }
-
-  s = deleted_files;
-  while (s != NULL) {
-    fprintf(stderr, "d: %s\n", s->path);
-    s = s->next;
-  }
-
-  free_listset(read_from_directories);
-  free_listset(read_from_files);
-  free_listset(written_to_files);
-  free_listset(deleted_files);
+  for (char *path = start_iterating(&read_from_directories);path;path = iterate(&read_from_directories))
+    fprintf(stderr, "l: %s\n", path);
+  for (char *path = start_iterating(&read_from_files);path;path = iterate(&read_from_files))
+    fprintf(stderr, "r: %s\n", path);
+  for (char *path = start_iterating(&written_to_files);path;path = iterate(&written_to_files))
+    fprintf(stderr, "w: %s\n", path);
+  for (char *path = start_iterating(&deleted_files);path;path = iterate(&deleted_files))
+    fprintf(stderr, "d: %s\n", path);
   return 0;
 }
