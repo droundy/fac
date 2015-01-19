f9010414fed52f701f3b687f35c7c3e426f27ad7
David Roundy <roundyd@physics.oregonstate.edu>
2015-01-19 09:05:34 -0800

    add test for two rules generating same output

---
+++ bugs/fail-on-two-rules-one-output.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -ev
+
+rm -rf $0.dir
+mkdir $0.dir
+cd $0.dir
+
+echo foo > foo
+echo bar > bar
+
+cat > top.bilge <<EOF
+| echo good > foobar
+> foobar
+
+| echo bad > foobar
+> foobar
+EOF
+
+if ../../bilge > bilge.out; then
+    cat bilge.out
+    echo This should not have passed
+    exit 1
+fi
+cat bilge.out
+
+exit 0
