0fbbbbb11055326b03ec0ef3d2fda9245c7dd486
David Roundy <roundyd@physics.oregonstate.edu>
2015-01-24 08:35:03 -0800

    more thorough cache tests

--- tests/cache-handling.sh
+++ tests/cache-handling.sh
@@ -18,11 +18,28 @@ c foobar
 > bar
 C /tmp
 
+| echo foo >  .cache-me && cat .cache-read > localcache
+> localcache
+C .cache
+
 | echo bar > foobar && echo foo > foo
 > foo
 c foobar
 EOF
 
+echo good > .cache-read
+
 ../../bilge
+
+cat top.bilge.done
+
+if grep '> .cache-me' top.bilge.done; then
+  echo this file should be ignored
+  exit 1
+fi
+if grep '< .cache-read' top.bilge.done; then
+  echo this file should be ignored
+  exit 1
+fi
 
 exit 0
