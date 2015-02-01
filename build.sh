419e61df863698d58347d6548e93c7d296ddabf2
David Roundy <roundyd@physics.oregonstate.edu>
2015-02-01 08:39:02 -0800

    update build script and add commented upload rule (not valid)

--- build.sh
+++ build.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
 
-gcc -Wall -Werror -O2 -std=c11 -g -c fac.c
+set -ev
 
-gcc -Wall -Werror -O2 -std=c11 -g -c files.c
+(gcc -Wall -Werror -O2 -std=c11 -g -c fac.c)
 
-gcc -Wall -Werror -O2 -std=c11 -g -c targets.c
+(gcc -Wall -Werror -O2 -std=c11 -g -c files.c)
 
-gcc -Wall -Werror -O2 -std=c11 -g -c clean.c
+(gcc -Wall -Werror -O2 -std=c11 -g -c targets.c)
 
-gcc -Wall -Werror -O2 -std=c11 -g -c new-build.c
+(gcc -Wall -Werror -O2 -std=c11 -g -c clean.c)
 
-gcc -Wall -Werror -O2 -std=c11 -g -c git.c
+(gcc -Wall -Werror -O2 -std=c11 -g -c new-build.c)
 
-cd lib && gcc -Wall -Werror -O2 -std=c11 -g -c trie.c
+(gcc -Wall -Werror -O2 -std=c11 -g -c git.c)
 
-cd lib && gcc -Wall -Werror -O2 -std=c11 -g -c listset.c
+(cd lib && gcc -Wall -Werror -O2 -std=c11 -g -c trie.c)
 
-cd lib && gcc -Wall -Werror -O2 -std=c11 -g -c iterablehash.c
+(cd lib && gcc -Wall -Werror -O2 -std=c11 -g -c listset.c)
 
-cd lib && gcc -Wall -Werror -O2 -std=c11 -g -c arrayset.c
+(cd lib && gcc -Wall -Werror -O2 -std=c11 -g -c iterablehash.c)
 
-python lib/get_syscalls.py /usr/src/linux-headers-3.2.0-4-common > lib/syscalls.h
+(cd lib && gcc -Wall -Werror -O2 -std=c11 -g -c arrayset.c)
 
-cd lib && gcc -Wall -Werror -O2 -std=c11 -g -c bigbrother.c
+(python lib/get_syscalls.py /usr/src/linux-headers-3.2.0-4-common > lib/syscalls.h)
 
-gcc -lpopt -lprofiler -o fac fac.o files.o targets.o clean.o new-build.o git.o lib/trie.o lib/listset.o lib/iterablehash.o lib/arrayset.o lib/bigbrother.o
+(cd lib && gcc -Wall -Werror -O2 -std=c11 -g -c bigbrother.c)
+
+(gcc -lpopt -lprofiler -o fac fac.o files.o targets.o clean.o new-build.o git.o lib/trie.o lib/listset.o lib/iterablehash.o lib/arrayset.o lib/bigbrother.o)
 
