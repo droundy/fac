26c4c1eaecb9a743885dda31f406b635513a6146
David Roundy <roundyd@physics.oregonstate.edu>
2015-02-07 11:43:11 -0800

    add another test for failure and fixing it

---
+++ tests/adding-missing-rule.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+set -ev
+
+echo $0
+
+# This test ensures that we can count builds properly to figure out if
+# we have built everything correctly.
+
+rm -rf $0.dir
+mkdir $0.dir
+cd $0.dir
+
+mkdir subdir
+
+cat > top.fac <<EOF
+| cat foo > bar
+< foo
+
+EOF
+
+git init
+git add top.fac
+
+echo hello > foo
+
+if ../../fac > fac.out; then
+    cat fac.out
+    echo this should have failed
+    exit 1
+fi
+cat fac.out
+
+grep 'add foo to git' fac.out
+
+cat >> top.fac <<EOF
+| echo hello > foo
+
+EOF
+
+../../fac
+
+grep hello bar
+
+exit 0
