1c7435dce4cce4a0726d31029da347453db256fb
David Roundy <roundyd@physics.oregonstate.edu>
2015-03-10 08:49:34 -0700

    clean up posixmodel test

--- tests/test-posix-model.c
+++ tests/test-posix-model.c
@@ -5,18 +5,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+int test_chdir(const char *dir, pid_t pid) {
+  printf("%5d: chdir %s\n", pid, dir);
+  if (model_chdir(0, dir, pid)) return 1;
+  if (dir[0] == '/') {
+    struct inode *cwd = model_cwd(pid);
+    char *dname = model_realpath(cwd);
+    printf("%5d: cwd -> %s\n", pid, dname);
+    if (strcmp(dir, dname)) {
+      printf("\nFAIL: %5d: %s != %s\n", pid, dname, dir);
+      return 1;
+    }
+    free(dname);
+  }
+  return 0;
+}
+
 int main(int argc, char **argv) {
   pid_t pid = 100;
-  int retval = model_chdir(0, "/test/directory", pid);
-
-  struct inode *cwd = model_cwd(pid);
-  char *dname = model_realpath(cwd);
-  printf("cwd gives %s\n", dname);
-  if (strcmp("/test/directory", dname)) {
-    retval |= 1;
-    printf("\nFAIL: %s should be %s\n", dname, "/test/directory");
-  }
-  free(dname);
+  int retval = test_chdir("/test/directory", pid);
 
   if (retval) {
     printf("Error: %d\n", retval);
