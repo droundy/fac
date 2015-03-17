209785602ad43902eb634263573a49482a7cfb47
David Roundy <roundyd@physics.oregonstate.edu>
2015-03-16 17:24:23 -0700

    sort entries so as to generate (more) reproducible scripts

--- build-linux.sh
+++ build-linux.sh
@@ -2,35 +2,35 @@
 
 set -ev
 
+(gcc -std=c11 -c clean.c)
+
+(gcc -std=c11 -c environ.c)
+
 (gcc -std=c11 -c fac.c)
 
 (gcc -std=c11 -c files.c)
 
-(gcc -std=c11 -c targets.c)
-
-(gcc -std=c11 -c clean.c)
-
-(gcc -std=c11 -c new-build.c)
-
 (gcc -std=c11 -c git.c)
-
-(gcc -std=c11 -c environ.c)
-
-(cd lib && gcc -std=c11 -c listset.c)
-
-(cd lib && gcc -std=c11 -c iterablehash.c)
-
-(cd lib && gcc -std=c11 -c sha1.c)
-
-(cd lib && gcc -std=c11 -c hashset.c)
-
-(cd lib && gcc -std=c11 -c posixmodel.c)
-
-(python2 lib/linux-syscalls.py > lib/linux-syscalls.h)
 
 (python2 lib/freebsd-syscalls.py > lib/freebsd-syscalls.h)
 
+(python2 lib/linux-syscalls.py > lib/linux-syscalls.h)
+
 (cd lib && gcc -std=c11 -c bigbrotheralt.c)
+
+(cd lib && gcc -std=c11 -c hashset.c)
+
+(cd lib && gcc -std=c11 -c iterablehash.c)
+
+(cd lib && gcc -std=c11 -c listset.c)
+
+(cd lib && gcc -std=c11 -c posixmodel.c)
+
+(cd lib && gcc -std=c11 -c sha1.c)
+
+(gcc -std=c11 -c new-build.c)
+
+(gcc -std=c11 -c targets.c)
 
 (gcc -lpopt -lpthread -o fac fac.o files.o targets.o clean.o new-build.o git.o environ.o lib/listset.o lib/iterablehash.o lib/sha1.o lib/hashset.o lib/posixmodel.o lib/bigbrotheralt.o)
 
