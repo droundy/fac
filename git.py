d8996422c0e4cbaba3eee21e2db6ea43e0420074
David Roundy <roundyd@physics.oregonstate.edu>
2015-03-23 14:47:31 -0400

    add a gpg public keyring to the repository (just experimenting)

---
+++ git.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" This is a small script that enables conveniently running git with
+a modified gnupg environment.  The purpose is to enable users to
+conveniently verify that commits and tags have been signed by actual
+authorized developers.  For more information, read the web page that I
+have not yet created. """
+
+import sys, subprocess, os
+
+if not os.path.exists('git.py'):
+    print('error: must call this program from its own directory')
+    exit(1)
+
+wd = os.getcwd()
+
+env = os.environ
+env['GNUPGHOME'] = wd+'/.gnupg'
+
+exit(subprocess.call(['git'] + sys.argv[1:], env=env))
