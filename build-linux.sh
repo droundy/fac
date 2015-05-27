ac41adb774559a5131e1dde58b2d5bd51566f7bd
David Roundy <roundyd@physics.oregonstate.edu>
2015-05-27 13:46:38 -0700

    fix to build on Debian oldstable

--- build-linux.sh
+++ build-linux.sh
@@ -14,29 +14,29 @@ set -ev
 
 (cd bigbro && rm -f libbigbro.a && ${AR-ar} rc libbigbro.a bigbro-linux.o && ${RANLIB-ranlib} libbigbro.a)
 
-(${CC-gcc} ${CFLAGS-} -std=c99 -Ibigbro -o clean.o -c clean.c)
+(${CC-gcc} ${CFLAGS-} -Ibigbro -std=c99 -o clean.o -c clean.c)
 
-(${CC-gcc} ${CFLAGS-} -std=c99 -Ibigbro -o environ.o -c environ.c)
+(${CC-gcc} ${CFLAGS-} -Ibigbro -std=c99 -o environ.o -c environ.c)
 
 (python3 generate-version-header.py > version-identifier.h)
 
-(${CC-gcc} ${CFLAGS-} -std=c99 -Ibigbro -o fac.o -c fac.c)
+(${CC-gcc} ${CFLAGS-} -Ibigbro -std=c99 -o fac.o -c fac.c)
 
-(${CC-gcc} ${CFLAGS-} -std=c99 -Ibigbro -o files.o -c files.c)
+(${CC-gcc} ${CFLAGS-} -Ibigbro -std=c99 -o files.o -c files.c)
 
-(${CC-gcc} ${CFLAGS-} -std=c99 -Ibigbro -o git.o -c git.c)
+(${CC-gcc} ${CFLAGS-} -Ibigbro -std=c99 -o git.o -c git.c)
 
-(cd lib && ${CC-gcc} ${CFLAGS-} -std=c99 -Ibigbro -o intmap.o -c intmap.c)
+(cd lib && ${CC-gcc} ${CFLAGS-} -Ibigbro -std=c99 -o intmap.o -c intmap.c)
 
-(cd lib && ${CC-gcc} ${CFLAGS-} -std=c99 -Ibigbro -o iterablehash.o -c iterablehash.c)
+(cd lib && ${CC-gcc} ${CFLAGS-} -Ibigbro -std=c99 -o iterablehash.o -c iterablehash.c)
 
-(cd lib && ${CC-gcc} ${CFLAGS-} -std=c99 -Ibigbro -o listset.o -c listset.c)
+(cd lib && ${CC-gcc} ${CFLAGS-} -Ibigbro -std=c99 -o listset.o -c listset.c)
 
-(cd lib && ${CC-gcc} ${CFLAGS-} -std=c99 -Ibigbro -o sha1.o -c sha1.c)
+(cd lib && ${CC-gcc} ${CFLAGS-} -Ibigbro -std=c99 -o sha1.o -c sha1.c)
 
-(${CC-gcc} ${CFLAGS-} -std=c99 -Ibigbro -o new-build.o -c new-build.c)
+(${CC-gcc} ${CFLAGS-} -Ibigbro -std=c99 -o new-build.o -c new-build.c)
 
-(${CC-gcc} ${CFLAGS-} -std=c99 -Ibigbro -o targets.o -c targets.c)
+(${CC-gcc} ${CFLAGS-} -Ibigbro -std=c99 -o targets.o -c targets.c)
 
-(${CC-gcc} -o fac fac.o files.o targets.o clean.o new-build.o git.o environ.o lib/listset.o lib/iterablehash.o lib/intmap.o lib/sha1.o ${LDFLAGS-} -lpopt -lpthread -lm -L/srv/home/droundy/src/fac/bigbro -lbigbro)
+(${CC-gcc} -o fac fac.o files.o targets.o clean.o new-build.o git.o environ.o lib/listset.o lib/iterablehash.o lib/intmap.o lib/sha1.o ${LDFLAGS-} -Lbigbro -lpopt -lpthread -lm -lbigbro)
 
