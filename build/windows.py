f5c5c7f27126b1dc81613688f46ae6d34c816777
David Roundy <roundyd@physics.oregonstate.edu>
2017-02-19 11:27:56 -0800

    build version-identifier.h in Windows build

--- build/windows.py
+++ build/windows.py
@@ -56,6 +56,8 @@ cfiles = ['arguments.c', 'build.c', 'clean-all.c', 'environ.c', 'fac.c', 'files.
 
 ofiles = [f[:-1]+'obj' for f in cfiles]
 
+subprocess.run('python generate-version-header.py > version-identifier.h', shell=True, check=True)
+
 # use cc for doing the linking
 cmd = ['x86_64-w64-mingw32-gcc', '-Ibigbro', '-o', 'fac.exe'] + cfiles + ['bigbro/libbigbro-windows.a', '-lpthread']
 print(' '.join(cmd))
