1a2012f33a902e5b11dc1443c95d4ca42212d7c2
David Roundy <roundyd@physics.oregonstate.edu>
2017-03-13 13:39:09 -0700

    add a couple more tests

---
+++ tests/generate-facfile-without-specifying-output.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -ev
+
+rm -rf $0.dir
+mkdir $0.dir
+cd $0.dir
+
+cat > my.fac <<EOF
+| echo foo bar > foo.fac
+EOF
+
+git init
+git add my.fac
+
+../../fac
+
+cat > my.fac <<EOF
+| echo foo bar > foo.fac
+> foo.fac
+EOF
+
+if ../../fac; then
+    echo should have crashed parsing foo.fac
+    exit 1
+fi
+
+
+exit 0
