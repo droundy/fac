9a2385f893e30548b9f1c6c9af755de1ebb160d0
David Roundy <roundyd@physics.oregonstate.edu>
2017-03-13 17:30:20 -0700

    new test to verify that fac does not run twice concurrently

---
+++ tests/fac-lock-file.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -ev
+
+if ! which screen; then
+    echo there is no screen available
+    exit 137
+fi
+
+rm -rf $0.dir
+mkdir $0.dir
+cd $0.dir
+
+cat > my.fac <<EOF
+| sleep 17 && echo good > good
+EOF
+
+git init
+git add my.fac
+
+../../fac &
+
+if ../../fac &> output; then
+    echo should have failed due to lock
+    cat output
+    exit 1
+fi
+
+cat output
+grep git/fac-lock output
+
+exit 0
