9a5e363be61a551298f933afc1545d05715f6419
David Roundy <roundyd@physics.oregonstate.edu>
2017-03-17 12:26:59 -0700

    make sparse.py work with older python3

--- sparse.py
+++ sparse.py
@@ -11,16 +11,15 @@ if myplatform != 'linux':
     print('# cannot use sparse on platform', myplatform)
     exit(0)
 
-ver = subprocess.run(['sparse', '--version'],
-                     stderr=subprocess.STDOUT,
-                     stdout=subprocess.PIPE)
-
-if ver.returncode != 0:
+try:
+    ver = subprocess.check_output(['sparse', '--version'],
+                                  stderr=subprocess.STDOUT)
+except:
     print('# there is no sparse present')
     exit(0)
 
 myver = b'v0.5.0-'
-if ver.stdout[:len(myver)] == myver:
+if ver[:len(myver)] == myver:
     print('# looking good!')
 else:
     print('# I am not confident with sparse version', ver.stdout)
