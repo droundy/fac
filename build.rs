4bc80d01e636067725e5c520f4be77a2b3d681d2
David Roundy <roundyd@physics.oregonstate.edu>
2017-05-15 07:38:29 -0700

    make rust build not fail when git fails to identify version

--- build.rs
+++ build.rs
@@ -7,15 +7,20 @@ fn main() {
     let mut stat = Command::new("git")
         .args(&["describe", "--dirty"])
         .output().unwrap();
-    if !stat.status.success() {
+    if !stat.status.success() || stat.stdout.len() < 5 {
         stat = Command::new("git")
             .args(&["revparse", "HEAD"])
             .output().unwrap();
     }
 
     let mut version = stat.stdout;
-    let newlen = version.len()-1;
-    version.truncate(newlen);
+    if version.len() > 1 {
+        let newlen = version.len()-1;
+        version.truncate(newlen);
+    }
+    if version.len() < 5 {
+        version.extend(b"(unknown version)");
+    }
 
     let mut file = std::fs::File::create("src/version.rs").expect("error creating version.rs");
     file.write(b"/// The version of fac\npub static VERSION: &'static str = \"")
