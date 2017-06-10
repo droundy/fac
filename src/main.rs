919cb93c789d388516c9eb468cdf664f0edd8a22
David Roundy <roundyd@physics.oregonstate.edu>
2017-06-10 14:50:23 -0700

    add profiler feature

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ extern crate quickcheck;
 #[macro_use]
 extern crate clap;
 
+#[cfg(feature="profile")]
+extern crate cpuprofiler;
+
 extern crate libc;
 extern crate ctrlc;
 
@@ -19,6 +22,19 @@ pub mod refset;
 pub mod build;
 mod version;
 
+#[cfg(feature="profile")]
+use cpuprofiler::PROFILER;
+
+#[cfg(feature="profile")]
+fn main() {
+    let flags = build::flags::args();
+    PROFILER.lock().unwrap().start("/tmp/fac.profile").unwrap();
+    let exitcode = build::build(flags);
+    PROFILER.lock().unwrap().stop().unwrap();
+    std::process::exit(exitcode);
+}
+
+#[cfg(not(feature="profile"))]
 fn main() {
     let flags = build::flags::args();
     let exitcode = build::build(flags);
