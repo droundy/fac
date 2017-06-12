7336ee98abad9fe8c88f240a9f0c1d48bbb29f90
David Roundy <roundyd@physics.oregonstate.edu>
2017-06-12 14:34:10 -0700

    copy the environment variable blacklist from C to rust

--- src/build/env.rs
+++ src/build/env.rs
@@ -15,8 +15,60 @@ pub fn hash() -> u64 {
         let mut env: Vec<_> = std::env::vars_os().collect();
         env.sort();
         let mut hasher = DefaultHasher::new();
+        for &(ref k,ref v) in &env {
+            if !IGNORE_THESE.contains(&k.to_string_lossy().as_ref()) {
+                k.hash(&mut hasher);
+                v.hash(&mut hasher);
+            }
+        }
         env.hash(&mut hasher);
         unsafe { ENVHASH = hasher.finish(); }
     };
     unsafe { ENVHASH }
 }
+
+const IGNORE_THESE: &'static [&'static str] = &[
+  "COLORTERM",
+  "DBUS_SESSION_BUS_ADDRESS",
+  "DESKTOP_SESSION",
+  "DESKTOP_STARTUP_ID",
+  "DISPLAY",
+  "GCONF_GLOBAL_LOCKS",
+  "GDM_LANG",
+  "GDMSESSION",
+  "GIO_LAUNCHED_DESKTOP_FILE_PID",
+  "GIO_LAUNCHED_DESKTOP_FILE",
+  "GJS_DEBUG_OUTPUT",
+  "GJS_DEBUG_TOPICS",
+  "GNOME_DESKTOP_SESSION_ID",
+  "GNOME_KEYRING_CONTROL",
+  "GNOME_KEYRING_PID",
+  "GPG_AGENT_INFO",
+  "OLDPWD",
+  "MAIL",
+  "NCURSES_",
+  "PS1",
+  "PWD",
+  "SESSION_MANAGER",
+  "SHLVL",
+  "SSH_AGENT_PID",
+  "SSH_AUTH_SOCK",
+  "SSH_CLIENT",
+  "SSH_CONNECTION",
+  "SSH_TTY",
+  "TERM",
+  "USERNAME", // not sure why, but this is sporadically defined
+  "VTE_VERSION",
+  "WINDOWID",
+  "WINDOWPATH",
+  "XAUTHORITY",
+  "XDG_CURRENT_DESKTOP",
+  "XDG_DATA_DIRS",
+  "XDG_MENU_PREFIX",
+  "XDG_SEAT",
+  "XDG_SESSION_COOKIE",
+  "XDG_SESSION_DESKTOP",
+  "XDG_SESSION_ID",
+  "XDG_VTNR",
+  "_",
+];
