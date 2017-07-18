eb9bd0fe29f0a24b381c873d717c9ace8d743c38
David Roundy <roundyd@physics.oregonstate.edu>
2017-07-18 10:20:35 -0700

    use cargo rather than cross for travis-ci

--- ci/script.sh
+++ ci/script.sh
@@ -9,16 +9,16 @@ main() {
     git describe --dirty || true
     git rev-parse HEAD
 
+    rustup install $TARGET
     cargo build --target $TARGET
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    cargo build --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET
-    cross test --target $TARGET --release
+    cargo test --target $TARGET
+    cargo test --target $TARGET --release
 
     # cross run --target $TARGET
     # cross run --target $TARGET --release
