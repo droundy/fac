81f827eb09fda160be82ba82e326abf62c7aaa83
David Roundy <roundyd@physics.oregonstate.edu>
2017-07-18 10:51:40 -0700

    start work on integration test through cargo

---
+++ tests/lib.rs
@@ -0,0 +1,10 @@
+extern crate fac;
+
+#[test]
+fn it_works() {
+    assert!(fac::version::VERSION.len() > 0);
+}
+
+// fn create_repository(files: std::collections::HashMap<std::path::PathBuf, Vec<u8>>) {
+    
+// }
