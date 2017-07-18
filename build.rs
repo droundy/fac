479bb8346e36a75467a41aef0726f4cfbb89d8f3
David Roundy <roundyd@physics.oregonstate.edu>
2017-07-18 07:51:45 -0700

    handle cargo build when git is not in path

--- build.rs
+++ build.rs
@@ -3,34 +3,34 @@ use std::process::Command;
 use std::io::{Write};
 
 macro_rules! print_err {
-    ($($arg:tt)*) => (
-        {
+    ($($arg:tt)*) => {{
             use std::io::prelude::*;
             write!(&mut ::std::io::stderr(), "{}\n", format_args!($($arg)*)).ok();
-        }
-    )
+    }}
 }
 
 fn main() {
-    print_err!("running git describe --dirty");
-    let mut stat = Command::new("git")
-        .args(&["describe", "--dirty"])
-        .output().expect("unable to call git describe?");
-    if !stat.status.success() || stat.stdout.len() < 5 {
-        print_err!("running revparse HEAD, since git describe --dirty failed");
-        stat = Command::new("git")
-            .args(&["revparse", "HEAD"])
-            .output().expect("unable to call git revparse?");
-    }
+    let mut version: Vec<u8>;
+    if let Ok(mut stat) = Command::new("git").args(&["describe", "--dirty"]).output() {
+        if !stat.status.success() || stat.stdout.len() < 5 {
+            print_err!("running revparse HEAD, since git describe --dirty failed");
+            stat = Command::new("git")
+                .args(&["revparse", "HEAD"])
+                .output().expect("unable to call git revparse?");
+        }
 
-    print_err!("constructing version string");
-    let mut version = stat.stdout;
-    if version.len() > 1 {
-        let newlen = version.len()-1;
-        version.truncate(newlen);
-    }
-    if version.len() < 5 {
-        version.extend(b"(unknown version)");
+        print_err!("constructing version string");
+        version = stat.stdout;
+        if version.len() > 1 {
+            let newlen = version.len()-1;
+            version.truncate(newlen);
+        }
+        if version.len() < 5 {
+            version.extend(b"(unknown version)");
+        }
+    } else {
+        print_err!("Cannot run git executable?!");
+        version = Vec::from("unknown");
     }
 
     // The following attempts to atomically create version.rs, in case
