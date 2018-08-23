27ec78afffd2351567095bca756f9a08587bbd10
David Roundy <roundyd@physics.oregonstate.edu>
2018-08-22 17:04:12 -0700

    remove actual C code (now unused)

--- build/clean-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -f dangling=true -q)
--- build/install.sh
+++
@@ -1,4 +0,0 @@
-set -ev
-
-cp fac-static $DESTDIR/usr/bin/fac
-cp fac.1 $DESTDIR/usr/share/man/man1/
