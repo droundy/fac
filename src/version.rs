6b58271e0889e402bdd90dd166b6b486f7b4b848
David Roundy <roundyd@physics.oregonstate.edu>
2019-05-01 05:58:02 -0700

    switch to using git-version rather than custom build.rs

--- src/version.rs
+++ src/version.rs
@@ -1,2 +1,2 @@
 /// The version of fac
-pub static VERSION: &'static str = include_str!(concat!(env!("OUT_DIR"), "/commit-info.txt"));
+pub const VERSION : &str = git_version::git_describe!("--always", "--dirty");
